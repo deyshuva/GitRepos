9f1177d4512510849fa491b0910c3c5522b18836
Shuva Dey <deyshuva93@gmail.com>
2022-01-07 21:59:11 -0500

    ArrayIndex
    Array Index position. Testing

---
+++ ArrayHW/ArrayHW/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ArrayHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter word");
+            string word = Console.ReadLine();
+            int n = word.Length;
+            //array
+            char[] myArray = new char [n];
+            //myArray = word.ToCharArray();
+
+            for(int i=0; i< n;i++)
+            {
+                myArray[i] = word[i];
+                Console.WriteLine(myArray[i]);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Index [{0}] = {1} ", i, myArray[i]);
+            }
+
+
+
+        }
+    }
+}
